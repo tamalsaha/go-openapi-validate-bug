3478e2b1870467932a05fb890d2b9d9ab73fefce
Tamal Saha <tamal@appscode.com>
2021-03-09 15:15:44 -0800

    added-all

--- go.mod
+++ go.mod
@@ -6,24 +6,7 @@ require (
 	github.com/go-openapi/spec v0.19.5
 	github.com/go-openapi/strfmt v0.19.5
 	github.com/go-openapi/validate v0.19.8
+	k8s.io/apiextensions-apiserver v0.20.4
+	k8s.io/apimachinery v0.20.4
+	sigs.k8s.io/yaml v1.2.0
 )
-
-replace github.com/go-openapi/analysis => github.com/go-openapi/analysis v0.19.5
-
-replace github.com/go-openapi/errors => github.com/go-openapi/errors v0.19.2
-
-replace github.com/go-openapi/jsonpointer => github.com/go-openapi/jsonpointer v0.19.3
-
-replace github.com/go-openapi/jsonreference => github.com/go-openapi/jsonreference v0.19.3
-
-replace github.com/go-openapi/loads => github.com/go-openapi/loads v0.19.4
-
-replace github.com/go-openapi/runtime => github.com/go-openapi/runtime v0.19.4
-
-replace github.com/go-openapi/spec => github.com/go-openapi/spec v0.19.3
-
-replace github.com/go-openapi/strfmt => github.com/go-openapi/strfmt v0.19.3
-
-replace github.com/go-openapi/swag => github.com/go-openapi/swag v0.19.5
-
-replace github.com/go-openapi/validate => github.com/go-openapi/validate v0.19.5
--- main.go
+++ main.go
@@ -1,27 +1,59 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
+
 	"github.com/go-openapi/spec"
-	"github.com/go-openapi/validate"
 	"github.com/go-openapi/strfmt"
+	"github.com/go-openapi/validate"
+	apiextensionsinternal "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apiservervalidation "k8s.io/apiextensions-apiserver/pkg/apiserver/validation"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/yaml"
 )
 
-func main2() {
-	openapiSchema := &spec.Schema{
-		SchemaProps: spec.SchemaProps{
-			ID: "",
-			Type: spec.StringOrArray{
-				"string",
-			},
-		},
-	}
-	validator := validate.NewSchemaValidator(openapiSchema, nil, "", strfmt.Default)
-	result := validator.Validate("")
-	fmt.Println(result.AsError())
-}
+var (
+	//go:embed cr.yaml
+	cr string
+
+	//go:embed crd.v1.yaml
+	crd string
+)
 
 func main() {
+	var crd1 apiextensionsv1.CustomResourceDefinition
+	err := yaml.Unmarshal([]byte(crd), &crd1)
+	if err != nil {
+		panic(err)
+	}
+	validationSchema := crd1.Spec.Versions[0].Schema
+
+	var internalValidationSchema *apiextensionsinternal.CustomResourceValidation
+	if validationSchema != nil {
+		internalValidationSchema = &apiextensionsinternal.CustomResourceValidation{}
+		if err := apiextensionsv1.Convert_v1_CustomResourceValidation_To_apiextensions_CustomResourceValidation(validationSchema, internalValidationSchema, nil); err != nil {
+			panic(fmt.Errorf("failed to convert CRD validation to internal version: %v", err))
+		}
+	}
+
+	var u unstructured.Unstructured
+	err = yaml.Unmarshal([]byte(cr), &u)
+	if err != nil {
+		panic(err)
+	}
+
+	schemaValidator, _, err := apiservervalidation.NewSchemaValidator(internalValidationSchema)
+	if err != nil {
+		panic(err)
+	}
+
+	err2 := apiservervalidation.ValidateCustomResource(nil, u.UnstructuredContent(), schemaValidator)
+	fmt.Println(err2.ToAggregate())
+}
+
+func main33() {
 	openapiSchema := &spec.Schema{
 		SchemaProps: spec.SchemaProps{
 			ID: "",
@@ -29,7 +61,7 @@ func main() {
 				"object",
 			},
 			Properties: map[string]spec.Schema{
-				"astr": spec.Schema{
+				"astr": {
 					SchemaProps: spec.SchemaProps{
 						Type: spec.StringOrArray{
 							"string",
